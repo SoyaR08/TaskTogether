8dc0e4a7daf707618641d66696d73bd3520d8d3a
SoyaR08 <rafa140200@gmail.com>
2025-06-04 08:23:39 +0200

    Arreglo BreadCrumb y subo cambios pre eliminación SSR

--- src/app/addmember/addmember.component.ts
+++ src/app/addmember/addmember.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addmember',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './addmember.component.html',
   styleUrl: './addmember.component.css'
 })
 export class AddmemberComponent {
+
+  private fb: FormBuilder = inject(FormBuilder)
+  newmemberForm = this.fb.group({
+    project: ['', [Validators.required]],
+    members: this.fb.array([])
+  })
+
+  newMember: FormControl = this.fb.control('', [Validators.required]); // Control para añadir el nuevo miembro
 
 }
