70900b042b77bb546a2313c4ff04d670ffaa24cd
SoyaR08 <rafa140200@gmail.com>
2025-06-07 20:15:52 +0200

    Ya se muestran los comentarios

--- src/app/modals/comments/comments.component.ts
+++ src/app/modals/comments/comments.component.ts
@@ -1,16 +1,19 @@
 import { Component, Inject, inject, OnInit, signal } from '@angular/core';
 import { CommentService } from '../../services/comment.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-comments',
-  imports: [],
+  imports: [NgFor, NgClass],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
 export class CommentsComponent implements OnInit{
 
   service: CommentService = inject(CommentService);
+  private fb: FormBuilder = inject(FormBuilder);
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { taskId: Number }) {}
 
