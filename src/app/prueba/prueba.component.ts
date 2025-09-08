efef149cb30fb3e44b37e10b7cc10bf091b78af2
SoyaR08 <rafa140200@gmail.com>
2025-09-08 19:14:17 +0200

    Check how it's going to be the new structure

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -2,10 +2,11 @@ import { NgClass, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LoginService } from '../services/login.service';
+import { NavbarComponent } from '../partials/navbar/navbar.component';
 
 @Component({
   selector: 'app-prueba',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './prueba.component.html'
 })
 export class PruebaComponent {
